10e8b979f514abaadf2c3f172b19ebe0bbb734ee
Priyanka <priyanka.rel6@gmail.com>
2025-06-11 10:28:29 +0530

    created Terraform files

--- variables/ec2-instance.tf
+++ variables/ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "roboshop" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    ami                      = var.ami_id
+    instance_type            = var.instance_type
+    vpc_security_group_ids   = [ aws_security_group.allow_all.id ]
 
     tags = var.ec2_tags
 }
