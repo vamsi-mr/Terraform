5656e87b0bb3315bd5ab5e8917c131945c1e6485
Priyanka <priyanka.rel6@gmail.com>
2025-06-11 09:22:05 +0530

    created Terraform files

---
+++ for-loop/Route53.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "www" {
+    #count   = 4
+    for_each = aws_instance.roboshop
+    zone_id = var.zone_id
+    name    = "${each.key}.${var.domain_name}" #mongodb.ravada.site
+    type    = "A"
+    ttl     = 1
+    records = [each.value.private_ip]
+}
