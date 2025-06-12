958b0bd00b0aabe8a31deee3ddf66a0e378b006d
Priyanka <priyanka.rel6@gmail.com>
2025-06-12 11:15:56 +0530

    created multi env file

---
+++ Terraform-Multi-Env/tfvars/Route53.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "www" {
+  count           = length(var.instances)
+  zone_id         = var.zone_id
+  name            = "${var.instances[count.index]}.${var.domain_name}" #mongodb.ravada.site
+  type            = "A"
+  ttl             = 1
+  records         = [aws_instance.roboshop[count.index].private_ip]
+  allow_overwrite = true
+}
