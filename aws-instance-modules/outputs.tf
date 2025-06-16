173fa408d42ba7c1cd1a4de4c2725ed5eef7c21d
Priyanka <priyanka.rel6@gmail.com>
2025-06-16 15:34:12 +0530

    created aws instance module file to create instances

--- aws-instance-modules/outputs.tf
+++ aws-instance-modules/outputs.tf
@@ -7,6 +7,6 @@ output "private_ip" {
 }
 
 output "instance_id" {
-    value = aws_instance.this.id
+  value = aws_instance.this.id
 }
 
--- aws-instance-modules/variable.tf
+++ aws-instance-modules/variable.tf
@@ -1,23 +1,23 @@
 variable "ami_id" {
-    type = string
-    default = "ami-09c813fb71547fc4f"
-    description = "AMI Id of join-devops practice instance"
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "AMI Id of join-devops practice instance"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 
-    validation {
-        condition = contains(["t2.micro", "t2.medium", "t3.micro"], var.instance_type)
-        error_message = "Valid values for instance_type are: t2.micro, t2.medium, t3.micro"
-        }
+  validation {
+    condition     = contains(["t2.micro", "t2.medium", "t3.micro"], var.instance_type)
+    error_message = "Valid values for instance_type are: t2.micro, t2.medium, t3.micro"
+  }
 }
 
 variable "sg_ids" {
-    type = list
+  type = list(any)
 }
 
 variable "tags" {
-    type = map
+  type = map(any)
 }
