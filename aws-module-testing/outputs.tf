ec35ea450964e4f241a3d110698177657e5aad2d
Priyanka <priyanka.rel6@gmail.com>
2025-06-16 14:08:41 +0530

    created aws instance module file to create instances

--- aws-module-testing/outputs.tf
+++ aws-module-testing/outputs.tf
@@ -1,3 +1,11 @@
-output "ec2_info" {
-  value = module.ec2_test
+output "public_ip" {
+  value = module.ec2_test.public_ip
+}
+
+output "private_ip" {
+  value = module.ec2_test.private_ip
+}
+
+output "instance_id" {
+  value = module.ec2_test.id
 }
