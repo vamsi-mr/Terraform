b957d7328592c5ea0d15b8e08ae7189500bf16d9
Priyanka <priyanka.rel6@gmail.com>
2025-06-17 20:34:59 +0530

    created aws vpc module to test

---
+++ aws-vpc-module-test/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "../aws-vpc-module"
+  project = "roboshop"
+  environment = "dev"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
+  database_subnet_cidrs = ["10.0.21.0/24", "10.0.22.0/24"]
+}
