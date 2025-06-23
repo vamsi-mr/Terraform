98542804ed7ec94f215703984cecf50e4f8c46bf
Priyanka <priyanka.rel6@gmail.com>
2025-06-23 12:21:22 +0530

    created route tables

--- aws-vpc-module-test/main.tf
+++ aws-vpc-module-test/main.tf
@@ -1,8 +1,9 @@
 module "vpc" {
   source                = "../aws-vpc-module"
-  project               = "roboshop"
-  environment           = "dev"
-  public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs  = ["10.0.11.0/24", "10.0.12.0/24"]
-  database_subnet_cidrs = ["10.0.21.0/24", "10.0.22.0/24"]
+  project               = var.project
+  environment           = var.environment
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required = true
 }
